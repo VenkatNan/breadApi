806e3b6714213e09f6a572b12e6a1854542cc367
VenkatNan <nanditavenkat@gmail.com>
2020-12-10 13:33:42 -0600

    Create and Show works

--- controllers/api/breads.js
+++ controllers/api/breads.js
@@ -16,9 +16,17 @@ function index(req,res){
   }
 
 function show(req,res){
-      
+      Bread.findById(req.params.id)
+      .then((bread)=>{
+        res.status(200).json(bread)
+      })
 }
+
 function create(req,res){
+    Bread.create(req.body)
+    .then((bread)=>{
+        res.status(201).json(bread)
+    })
       
 }
 function update(req,res){
